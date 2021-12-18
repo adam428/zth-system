b0a9fdfd66d24da1e8135cae2ec313baa270cbeb
Adam Koziel <ylabel3@gmail.com>
2021-12-18 22:35:07 +0100

    xx

---
+++ src/main/java/com/example/zthsystem/ZthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.zthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZthSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zthsystem/ZthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZthSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
