5037ee8a91c3d925df80fdc4971ea111a9972eef
Adam Koziel <ylabel3@gmail.com>
2021-12-18 22:39:59 +0100

    test

--- src/main/java/com/example/zthsystem/ZthSystemApplication.java
+++ src/main/java/com/example/zthsystem/ZthSystemApplication.java
@@ -2,11 +2,22 @@ package com.example.zthsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class ZthSystemApplication {
 
+    @RequestMapping("/zthsystem")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(ZthSystemApplication.class, args);
     }
 
