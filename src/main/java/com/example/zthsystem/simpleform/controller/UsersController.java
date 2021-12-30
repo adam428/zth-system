0c3990abe3011a64d50ec9b4a4d138f2c8e7244a
ItsmeDominik <64275958+ItsmeDominik@users.noreply.github.com>
2021-12-30 18:30:35 +0100

    login-registry_form-part3

--- src/main/java/com/example/zthsystem/simpleform/controller/UsersController.java
+++ src/main/java/com/example/zthsystem/simpleform/controller/UsersController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UsersController {
 
-    @GetMapping("/register")
+    @GetMapping("/register_page")
     public String getRegisterPage(){
         return "register_page";
     }
-    @GetMapping("/login")
+    @GetMapping("/login_page")
     public String getLoginPage(){
         return "login_page";
     }
